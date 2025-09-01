19553894ba1027344dacf5cc3237e5e46393a6ad
piechu007 <dpiech007@gmail.com>
2025-09-01 11:40:19 +0200

    [Added] Create and Init AbilitySystemComponent for Aura and Enemy

---
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/AuraPlayerState.h"
+
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+
+AAuraPlayerState::AAuraPlayerState()
+{
+    AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>("AbilitySystemComponent");
+    AbilitySystemComponent->SetIsReplicated(true);
+    AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Mixed);
+
+    AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>("AttributeSet");
+    
+    SetNetUpdateFrequency(100.0f);
+}
+UAbilitySystemComponent *AAuraPlayerState::GetAbilitySystemComponent() const
+{
+    return AbilitySystemComponent;
+}
+
+UAttributeSet *AAuraPlayerState::GetAttributeSet() const
+{
+    return AttributeSet;
+}
